6f4ca781e9f5603eddb86308f9b1147317622987
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2016-11-08 18:21:27 +0000

    Updated the firmware version number.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@11455 06ee2f96-5407-11de-8bab-f3d7589a4122

--- demos/stream_framing_responder/responder.c
+++ demos/stream_framing_responder/responder.c
@@ -43,7 +43,7 @@
 #include "libstream.h"
 #include "messages.h"
 
-#define FIRMWARE_VERSION	11449
+#define FIRMWARE_VERSION	11455
 
 //***************************************************************************
 
