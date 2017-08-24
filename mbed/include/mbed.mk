e1c8cb2f053d526ff49be3db28b9d9e57501b0fe
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2017-08-24 11:31:50 +0000

    Added makefiles for using mbed OS CLI.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@13184 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ mbed/include/mbed.mk
@@ -0,0 +1,64 @@
+# Common make definitions for building mbed OS applications
+
+# Copyright (C)2017, Philip Munts, President, Munts AM Corp.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# To install the common mbed OS libraries, either install Debian package
+# munts-mbed-cli, or run the following commands:
+#
+# sudo pip install mbed-cli
+# sudo mbed new /usr/local/lib/mbed-cli
+
+MBEDCLIPATH	?= /usr/local/lib/mbed-cli
+OUTPUTPATH	?= ./BUILD/$(BOARDNAME)/$(TOOLCHAINNAME)
+
+# Default target placeholder
+
+mbed_mk_default: default
+
+# Prepare for mbed compile
+
+prepare.done:
+	ln -s $(MBEDCLIPATH)/mbed-os
+	ln -s $(MBEDCLIPATH)/mbed-os.lib
+	ln -s $(MBEDCLIPATH)/mbed_settings.py
+	touch $@
+
+# Perform mbed compile
+
+compile.done: prepare.done
+	mbed compile $(MBEDFLAGS)
+	touch $@
+
+mbed_mk_build: compile.done
+
+# Remove working files
+
+mbed_mk_clean:
+	-rm -rf BUILD compile.done
+
+mbed_mk_reallyclean: mbed_mk_clean
+	-rm -f mbed-os
+	-rm -f mbed-os.lib
+	-rm -f mbed_settings.py
+	-rm -f mbed_settings.pyc
+	-rm -f prepare.done
+
+mbed_mk_distclean: mbed_mk_reallyclean
