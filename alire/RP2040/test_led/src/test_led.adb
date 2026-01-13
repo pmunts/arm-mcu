9391598b2b63311bcde996511589fc0528d7964c
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-01-13 23:26:27 +0000

    Checkpoint WIP commit.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24190 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ alire/RP2040/test_led/src/test_led.adb
@@ -0,0 +1,45 @@
+-- SparkFun Pro Micro RP2040 LED Test 
+-- Copyright (C)2026, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH RP.Clock;
+WITH RP.GPIO;
+WITH SparkFun_Pro_Micro_RP2040;
+WITH USBSerial; USE USBSerial;
+
+PROCEDURE test_led IS
+
+  LED RENAMES SparkFun_Pro_Micro_RP2040.GP2;
+
+BEGIN
+  RP.Clock.Initialize(SparkFun_Pro_Micro_RP2040.Crystal);
+  USBSerial.Initialize;
+
+  New_Line;
+  Put_Line("SparkFun Pro Micro RP2040 LED Test");
+  New_Line;
+
+  LED.Configure(RP.GPIO.Output);
+
+  LOOP
+    LED.Toggle;
+    Delay_Milliseconds(500); 
+  END LOOP;
+END test_led;
