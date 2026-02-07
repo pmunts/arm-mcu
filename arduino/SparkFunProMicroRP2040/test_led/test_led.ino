cff2992f6cb1d66bc0fc086ab3de853b4a2b36f6
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-07 16:32:26 +0000

    Added to repository.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24343 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ arduino/SparkFunProMicroRP2040/test_led/test_led.ino
@@ -0,0 +1,46 @@
+// Simplest Arduino Example Program: Flash an LED
+
+// Copyright (C)2026, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Hardware configuration (bottom to top):
+//
+// Sparkfun Electronics Pro Micro - RP2040  (3.3V logic!)
+
+// GPIO pin assignments:
+
+const int LED = 2;
+
+// Initialization Code (run once at startup)
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+// Event Loop (run repeatedly forever)
+
+void loop()
+{
+  // Toggle the LED
+  digitalWrite(LED, !digitalRead(LED));
+  // Wait a while
+  delay(100);
+}
