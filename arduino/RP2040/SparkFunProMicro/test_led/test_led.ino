c6af18f7cb1162b98edc3b034316b0632455f629
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-15 20:48:25 +0000

    Renamed SparfunProMicroRP2050/ to just SparkfunProMicro.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24407 06ee2f96-5407-11de-8bab-f3d7589a4122

--- arduino/RP2040/SparkFunProMicro/test_led/test_led.ino
+++ arduino/RP2040/SparkFunProMicro/test_led/test_led.ino
@@ -20,6 +20,8 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include <Arduino.h>
+
 // Hardware configuration (bottom to top):
 //
 // Sparkfun Electronics Pro Micro - RP2040  (3.3V logic!)
