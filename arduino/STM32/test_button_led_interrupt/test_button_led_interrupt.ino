e5620539b96df9deb779cd69a2d223126fafaf49
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-16 21:21:30 +0000

    Imported button and LED tests from AVR/arduino/
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24433 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ arduino/STM32/test_button_led_interrupt/test_button_led_interrupt.ino
@@ -0,0 +1,54 @@
+// Arduino Button and LED Test
+
+// Copyright (C)2025-2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Hardware configuration (bottom to top):
+//
+// Nucleo STM32F411RE (3.3V logic!)
+
+#include <Arduino.h>
+
+// Button Input Interrupt Service Routine
+
+void EdgeHandler(void)
+{
+  bool newstate = !digitalRead(USER_BTN);
+  digitalWrite(LED_BUILTIN, newstate);
+}
+
+void setup()
+{
+  Serial2.begin(115200);
+  Serial2.println("\ecArduino Button and LED Test\n");
+
+  pinMode(USER_BTN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, !digitalRead(USER_BTN));
+
+  attachInterrupt(digitalPinToInterrupt(USER_BTN), EdgeHandler, CHANGE);
+}
+
+void loop()
+{
+  Serial2.println("Tick...");
+  delay(1000);
+}
