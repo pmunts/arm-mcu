fc81e5688546295411d2e87db61fba097eaacee6
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-20 08:34:37 +0000

    Imported test_button_led_freertos
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24484 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ arduino/RP2040/SparkFunProMicro/test_button_led_freertos/test_button_led_freertos.ino
@@ -0,0 +1,91 @@
+// Arduino RP2040 Button and LED Test Using FreeRTOS
+
+// Copyright (C)2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <Arduino.h>
+#define __FREERTOS 1
+#include <FreeRTOS.h>
+
+// Assign GPIO pins
+
+#define LED	2
+#define BUTTON	3
+
+QueueHandle_t EdgeQueue;
+
+// Button Interrupt Service Routine -- Enqueue button transitions
+
+void EdgeHandler(void)
+{
+  bool newstate = !digitalRead(BUTTON);
+  xQueueSendFromISR(EdgeQueue, &newstate, NULL);
+}
+
+// Main task -- Dequeue button transitions and display results
+
+void MainTaskFunction(void *parameters)
+{
+  // Attach button GPIO pin interrupt service routine
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON), EdgeHandler, CHANGE);
+
+  // Main event loop
+
+  for (;;)
+  {
+    bool newstate;
+
+    if (xQueueReceive(EdgeQueue, &newstate, pdMS_TO_TICKS(1000)) == pdPASS)
+    {
+      Serial.println(newstate ? "PRESS" : "RELEASE");
+      digitalWrite(LED, newstate);
+    }
+    else
+      Serial.println("Tick...");
+  }
+}
+
+// Configure hardware and start FreeRTOS.
+// **Does not return because vTaskStartScheduler() does not return!**
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("\ecArduino RP2040 Button and LED Test Using FreeRTOS\n");
+
+  pinMode(BUTTON, BUTTON_MODE);
+  pinMode(LED, OUTPUT);
+
+  digitalWrite(LED, !digitalRead(BUTTON));
+
+  // Create FreeRTOS entities and start the scheduler
+
+  EdgeQueue = xQueueCreate(10, sizeof(bool));
+  xTaskCreate(MainTaskFunction, "main", 512, NULL, 1, NULL);
+  vTaskStartScheduler();
+}
+
+// With FreeRTOS running, loop() is called by the idle task instead of the
+// Arduino main() function (which will block because setup() does not return).
+
+void loop()
+{
+}
