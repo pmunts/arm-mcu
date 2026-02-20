738360125b3184ed6b8b3b41173a36a6e134f483
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-20 19:00:41 +0000

    Added hardware configuration information.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24490 06ee2f96-5407-11de-8bab-f3d7589a4122

--- arduino/RP2040/SparkFunProMicro/test_button_led_freertos/test_button_led_freertos.ino
+++ arduino/RP2040/SparkFunProMicro/test_button_led_freertos/test_button_led_freertos.ino
@@ -20,6 +20,10 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+// Hardware configuration (bottom to top):
+//
+// Sparkfun Electronics Pro Micro - RP2040  (3.3V logic!)
+
 #include <Arduino.h>
 #include <FreeRTOS.h>
 
