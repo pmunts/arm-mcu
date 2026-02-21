3b11629bed8f8dbae17ba22f88b62382b96bbb44
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-21 22:25:40 +0000

    Added servo over PWM support
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24504 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ arduino/libraries/MuntsTechnologies/ServoPWM.h
@@ -0,0 +1,78 @@
+// Servo output services using PWM services
+
+// Copyright (C)2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN N../../../RP2040/libraries/MuntsTechnologies/RP2040HardwarePWM.hO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _MUNTSTECH_SERVO_PWM_H
+#define _MUNTSTECH_SERVO_PWM_H
+
+#include <assert.h>
+#include <pwm-interface.h>
+#include <servo-interface.h>
+
+namespace MuntsTech::ServoPWM
+{
+  struct Output: public MuntsTech::Interfaces::Servo::Output_Interface
+  {
+    // Servo output constructor
+
+    Output(MuntsTech::Interfaces::PWM::Output pwmout, unsigned frequency,
+      double position = MuntsTech::Interfaces::Servo::POSITION_NEUTRAL,
+      double minwidth = 1.0E-3, double maxwidth = 2.0E-3)
+    {
+      assert(pwmout != NULL);
+      assert(frequency > 0);
+      assert(position >= MuntsTech::Interfaces::Servo::POSITION_MIN);
+      assert(position <= MuntsTech::Interfaces::Servo::POSITION_MAX);
+      assert(maxwidth > minwidth);
+      assert(maxwidth < 1.0/frequency);
+      this->pwmout   = pwmout;
+      this->freq     = frequency;
+      this->swing    = (maxwidth - minwidth)/2.0;
+      this->midpoint = minwidth + swing;
+      this->write(position);
+    }
+
+    // Servo output methods
+
+    virtual void write(const double position)
+    {
+      assert(position >= MuntsTech::Interfaces::Servo::POSITION_MIN);
+      assert(position <= MuntsTech::Interfaces::Servo::POSITION_MAX);
+      this->pwmout->write((this->midpoint + this->swing*position)*this->freq*100.0);
+    }
+
+    // Servo output operators
+
+    virtual void operator =(const double position)
+    {
+      this->write(position);
+    }
+
+  private:
+
+    MuntsTech::Interfaces::PWM::Output pwmout;
+    double freq;
+    double swing;
+    double midpoint;
+  };
+}
+
+#endif
