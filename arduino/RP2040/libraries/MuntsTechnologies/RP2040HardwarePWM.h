de8adc38964d5bd67cbb1768b20d5035c784addd
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-02-21 18:21:26 +0000

    Added support for RP2040 hardware PWM
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@24499 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ arduino/RP2040/libraries/MuntsTechnologies/RP2040HardwarePWM.h
@@ -0,0 +1,73 @@
+// PWM output services using RP2040 Hardware PWM Library
+//
+// See also:
+//
+// https://docs.arduino.cc/libraries/rp2040_pwm
+// https://github.com/khoih-prog/RP2040_PWM
+
+// Copyright (C)2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _MUNTSTECH_RP2040_PWM_H
+#define _MUNTSTECH_RP2040_PWM_H
+
+#include <RP2040_PWM.h>
+#include <pwm-interface.h>
+
+namespace MuntsTech::RP2040::HardwarePWM
+{
+  // PWM output class definition
+
+  struct Output: public MuntsTech::Interfaces::PWM::Output_Interface
+  {
+    // PWM output Constructor
+
+    Output(unsigned pin, unsigned frequency,
+      double dutycycle = MuntsTech::Interfaces::PWM::DUTYCYCLE_MIN)
+    {
+      this->pin  = pin;
+      this->freq = frequency;
+      this->outp = new RP2040_PWM(pin, frequency, dutycycle);
+      this->outp->setPWM();
+    }
+
+    // PWM output methods
+
+    virtual void write(const double dutycycle)
+    {
+      this->outp->setPWM(this->pin, this->freq, dutycycle);
+    }
+
+    // PWM output operators
+
+    virtual void operator =(const double dutycycle)
+    {
+      this->write(dutycycle);
+    }
+
+  private:
+
+    unsigned pin;
+    unsigned freq;
+    RP2040_PWM *outp;
+  };
+}
+
+#endif
---
+++ arduino/libraries/MuntsTechnologies/servo-interface.h
@@ -0,0 +1,51 @@
+// Abstract interface for RC (Radio Controlled) analog servo outputs
+
+// Copyright (C)2018-2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _MUNTSTECH_SERVO_INTERFACE_H
+#define _MUNTSTECH_SERVO_INTERFACE_H
+
+namespace MuntsTech::Interfaces::Servo
+{
+  // Normalized servo position constants
+
+  static constexpr double POSITION_MIN     = -1.0;
+  static constexpr double POSITION_NEUTRAL = 0.0;
+  static constexpr double POSITION_MAX     = 1.0;
+
+  struct Output_Interface
+  {
+    // Servo output methods
+
+    virtual void write(const double position) = 0;
+
+    // Servo output operators
+
+    void Output_Interface::operator =(const double position)
+    {
+      this->write(position);
+    }
+  };
+
+  typedef Output_Interface *Output;
+}
+
+#endif
