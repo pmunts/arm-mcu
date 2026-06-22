3a4c43d4e7bb01e9f9237fe8bb8f2b041dc755b9
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-06-22 04:03:51 +0000

    Don't hard code project GUID
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@25051 06ee2f96-5407-11de-8bab-f3d7589a4122

--- tinyclr/templates/munts_tinyclr.d/AssemblyInfo.cs
+++ tinyclr/templates/munts_tinyclr.d/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("efa830b8-7669-4a53-9a9c-9ffe0789c1b6")]
+[assembly: Guid("$guid1$")]
 
 // Version information for an assembly consists of the following four values:
 //
